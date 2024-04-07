e7549c2e56d245c93a2110d0b7a1d958379f60ef
avilwock <151412286+avilwock@users.noreply.github.com>
2024-04-07 14:43:32 -0700

    fixed seeds file

--- seeds/seeds.js
+++ seeds/seeds.js
@@ -1,33 +1,38 @@
 const sequelize = require('../config/connection.js');
-const { User, Post, Comment, Car } = require('../models'); //Replace with amanda's information
+const { User, Post, Comment, Car } = require('../models');
 
 const userData = require('./userData.json');
-const postData = require('./postData.json');//replace with the next file ont his seeds folder
-const commentData = require('./commentData.json');//replace with the next file ont his seeds folder
-const carData = require('./carData.json');//replace with the next file ont his seeds folder
-
-
+const postData = require('./postData.json');
+const commentData = require('./commentData.json');
+const carData = require('./carData.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
 
-  // Create car records
+  // Create users
   const users = await User.bulkCreate(userData, {
     individualHooks: true,
     returning: true,
   });
 
-  // Update user data with correct car_id
-  const updatedCarData = carData.map((car, index) => {
-    if (car.user_id) {
-      car.user_id = users[index].id;
-    }
-    return car;
-  });
+  // Create posts
+  const posts = await Post.bulkCreate(postData);
 
-  // Create users with updated data
-await Car.bulkCreate(updatedCarData);
-  // Your existing code to create posts can follow here
+  // Create comments
+  const comments = await Comment.bulkCreate(commentData);
+
+  // Assign posts and comments to users
+  await Promise.all(users.map(async (user, index) => {
+    if (postData[index]) {
+      await user.createPost(postData[index]);
+    }
+    if (commentData[index]) {
+      await user.createComment(commentData[index]);
+    }
+  }));
+
+  // Create cars
+  await Car.bulkCreate(carData);
 
   process.exit(0);
 };
