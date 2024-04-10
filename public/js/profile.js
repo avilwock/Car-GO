d806fb4db3e14bab162fb0a6f94e1f91f2d48733
Marisol514 <145890422+Marisol514@users.noreply.github.com>
2024-04-09 21:54:31 -0700

    updated button functionality and and added cludinary code

--- public/js/profile.js
+++ public/js/profile.js
@@ -33,6 +33,9 @@ document.addEventListener('DOMContentLoaded', function() {
     newPostForm.addEventListener('submit', function(event) {
       event.preventDefault();
       const title = document.getElementById('post-title').value;
+        
+      //TODO: got get image url off screen and grab SRC pass along. 
+      
       const content = document.getElementById('post-content').value;
       // Do something with the title and content, e.g., send them to the server
       console.log("New post created with title:", title, "and content:", content);
@@ -52,3 +55,49 @@ document.addEventListener('DOMContentLoaded', function() {
       }
     });
 });
+
+
+//TODO: 
+const cloudName = "hzxyensd5"; // replace with your own cloud name
+const uploadPreset = "aoh4fpwm"; // replace with your own upload preset
+
+// Remove the comments from the code below to add
+// additional functionality.
+// Note that these are only a few examples, to see
+// the full list of possible parameters that you
+// can add see:
+//   https://cloudinary.com/documentation/upload_widget_reference
+
+const myWidget = cloudinary.createUploadWidget(
+  {
+    cloudName: cloudName,
+    uploadPreset: uploadPreset,
+    // cropping: true, //add a cropping step
+    // showAdvancedOptions: true,  //add advanced options (public_id and tag)
+    // sources: [ "local", "url"], // restrict the upload sources to URL and local files
+    // multiple: false,  //restrict upload to a single file
+    // folder: "user_images", //upload files to the specified folder
+    // tags: ["users", "profile"], //add the given tags to the uploaded files
+    // context: {alt: "user_uploaded"}, //add the given context data to the uploaded files
+    // clientAllowedFormats: ["images"], //restrict uploading to image files only
+    // maxImageFileSize: 2000000,  //restrict file size to less than 2MB
+    // maxImageWidth: 2000, //Scales the image down to a width of 2000 pixels before uploading
+    // theme: "purple", //change to a purple theme
+  },
+  (error, result) => { //when completed
+    if (!error && result && result.event === "success") {
+      console.log("Done! Here is the image info: ", result.info);
+      document//looking for element on screen
+        .getElementById("uploadedimage")
+        .setAttribute("src", result.info.secure_url); //upliaded url for database 
+    }
+  }
+);
+
+document.getElementById("upload_widget").addEventListener(
+  "click",
+  function () {
+    myWidget.open();
+  },
+  false
+);
