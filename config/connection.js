821fee9526f401fd8963dd4863b57b1ad82d57c5
avilwock <151412286+avilwock@users.noreply.github.com>
2024-06-25 16:18:12 -0700

    next attempt at fixing render

--- config/connection.js
+++ config/connection.js
@@ -1,26 +1,18 @@
-// Import Sequelize
+// Imports Sequelize
 const Sequelize = require('sequelize');
-// Require dotenv for security
+// Requires .env for security
 require('dotenv').config();
 
-// Create a variable to hold the sequelize instance
-let sequelize;
-
 // Establish Sequelize connection based on environment variables
-sequelize = new Sequelize(
+const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
   process.env.DB_PASSWORD,
   {
-    host: process.env.DB_HOST,
+    host: 'localhost',
     dialect: 'mysql',
-    port: process.env.DB_PORT || 3306,
-    pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000
-    }
+    port: 3306,
+    logging: false // Disable logging (optional)
   }
 );
 
