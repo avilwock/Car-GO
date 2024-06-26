92b1adf71fd2b6a2e8998cc498b28351b23616eb
avilwock <151412286+avilwock@users.noreply.github.com>
2024-06-26 15:19:55 -0700

    trying to fix

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,25 @@
+//Imports sequelize
 const Sequelize = require('sequelize');
+//requires .env for security
+require('dotenv').config();
 
-const sequelize = new Sequelize({
-  dialect: 'mysql',
-  database: process.env.DB_NAME,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT, // Ensure to remove this line if not needed
-});
+
+//ensures that jawsdb_url is set to allow for use with a site like Heroku
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+  //if not using heroku, then it uses these default variables and values
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306
+    }
+  );
+}
 
 module.exports = sequelize;
+///= added comment to push folder"
