24273250506e7ed26d32318a015d4493dea4e0cd
avilwock <151412286+avilwock@users.noreply.github.com>
2024-06-25 18:02:20 -0700

    attempting to fix again

--- config/connection.js
+++ config/connection.js
@@ -1,25 +1,21 @@
-//Imports sequelize
 const Sequelize = require('sequelize');
-//requires .env for security
 require('dotenv').config();
 
+let sequelize;
 
-//ensures that jawsdb_url is set to allow for use with a site like Heroku
-if (process.env.DATABASE_URL) {
-  sequelize = new Sequelize(process.env.DATABASE_URL);
-  //if not using heroku, then it uses these default variables and values
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
   sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
-      host: process.env.DB_HOST,
+      host: process.env.DB_HOST || 'localhost',
       dialect: 'mysql',
-      port: 3306
+      port: process.env.DB_PORT || 3306
     }
   );
 }
 
 module.exports = sequelize;
-///= added comment to push folder"
